92d81706bcaeb76a29dea9b293154b8538ea9c79
Katharine Chui <kwchuiaa@connect.ust.hk>
2025-08-10 12:46:46 +0200

    use sema for some locking situations

--- pspnet_inet_kermit_redirect/kermit.h
+++ pspnet_inet_kermit_redirect/kermit.h
@@ -5,7 +5,8 @@
 
 #include "../kermit_common.h"
 
-extern SceLwMutexWorkarea request_slots_mutex;
+extern SceUID request_slots_mutex;
+
 uint64_t _kermit_send_request(uint32_t mode, uint32_t cmd, int num_args, int nbio, ...);
 #define kermit_send_request(mode, cmd, nbio, ...) _kermit_send_request(mode, cmd, sizeof((uint64_t[]){__VA_ARGS__}) / sizeof(uint64_t), nbio, __VA_ARGS__)
 #define kermit_send_wlan_request(...) kermit_send_request(KERMIT_MODE_WLAN, __VA_ARGS__)
